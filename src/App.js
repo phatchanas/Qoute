2a53d38f1893a1d1dc8d518ec2818da71b9cee6a
phatchanas <phatchanasrinin@gmail.com>
2020-06-04 16:45:17 +0200

    change h1 font

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <div className="App">
       
       <WiredCard fill="#F5F5F5" elevation={3}>      
-       <h1>InstaDeep</h1>
+       <h1>Need a Deep Instagram Quote?</h1>
       </WiredCard>
       <Quote quote={quote}/>
       <QuotesApi setQuote={setQuote}/>
