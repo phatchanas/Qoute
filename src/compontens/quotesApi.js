aa0056d97fd6909adb438bc3e726d151072ce773
phatchanas <phatchanasrinin@gmail.com>
2020-06-05 12:36:05 +0200

    added error and loading function

--- src/compontens/quotesApi.js
+++ src/compontens/quotesApi.js
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Axios from 'axios';
 import { WiredCard, WiredButton } from 'react-wired-elements';
 
 
 
 
+
 export default (props) => {
-    
-    
-    const url = `https://favqs.com/api/qotd`;
+    const [isLoading, setIsLoading] = useState(false)
+    const url = `http://favqs.com/api/qotd`;
 
     const getData = async () => {
         //fetch data from a url
+        setIsLoading(true)
         const result = await Axios.get(url)
-           props.setQuote(result.data.quote)         
-          }
+        .then(result => {
+            props.setQuote(result.data.quote)   
+            setIsLoading(false)        
+            }
+        )
+        .catch(error => {
+            setIsLoading(false)   
+            console.warn(error)
+       })
+        }   
 
           return (
               
               // wired-element button that return the result from the api
             <div>
-             <WiredCard fill="#F5F5F5" elevation={3}>
-            <WiredButton onClick={getData}>Click</WiredButton>
-            </WiredCard>
-           
-                
+                <WiredCard fill="#F5F5F5" elevation={3}>
+                <WiredButton onClick={getData}>Click</WiredButton>
+                </WiredCard>
+                {isLoading && <h1>Loading..</h1>}   
             </div>
             
   
